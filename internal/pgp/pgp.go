e05562dd6f7dc9e24325329da2da67c106a5691d
esote <esote.net@gmail.com>
2019-08-09 16:52:25 -0500

    PGP operations, unit tests

---
+++ internal/pgp/pgp.go
@@ -0,0 +1,115 @@
+package pgp
+
+import (
+	"bytes"
+	"crypto"
+	"crypto/rand"
+	"encoding/hex"
+	"io"
+
+	"golang.org/x/crypto/openpgp"
+	"golang.org/x/crypto/openpgp/armor"
+	"golang.org/x/crypto/openpgp/packet"
+)
+
+const (
+	nonceLen = 1024
+
+	// NonceHexLen is the length of a hexadecimal nonce in bytes.
+	NonceHexLen = 2 * nonceLen
+)
+
+// EncryptArmored encrypts plaintext for recipients by proving a plaintext and
+// list of armored public keys. Returns an armored, encrypted PGP message.
+//
+// The list of recipients is hidden, but the number of recipients can still be
+// determined.
+func EncryptArmored(public, plain io.Reader) ([]byte, error) {
+	key, err := openpgp.ReadArmoredKeyRing(public)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// Use speculative key IDs to hide encryption recipients. Countermeasure
+	// against traffic analysis.
+	for _, entity := range key {
+		entity.PrimaryKey.KeyId = 0
+
+		for _, subkey := range entity.Subkeys {
+			subkey.PublicKey.KeyId = 0
+		}
+	}
+
+	config := &packet.Config{
+		DefaultCipher: packet.CipherAES256,
+		DefaultHash:   crypto.SHA512,
+	}
+
+	hints := &openpgp.FileHints{
+		IsBinary: true,
+	}
+
+	var encrypted bytes.Buffer
+
+	wc, err := openpgp.Encrypt(&encrypted, key, nil, hints, config)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if _, err = io.Copy(wc, plain); err != nil {
+		return nil, err
+	}
+
+	if err = wc.Close(); err != nil {
+		return nil, err
+	}
+
+	var armored bytes.Buffer
+
+	wc, err = armor.Encode(&armored, "PGP MESSAGE", nil)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if _, err = encrypted.WriteTo(wc); err != nil {
+		return nil, err
+	}
+
+	if err = wc.Close(); err != nil {
+		return nil, err
+	}
+
+	return armored.Bytes(), nil
+}
+
+// NonceHex generates a random nonce encoded as hex.
+func NonceHex() (nonce [NonceHexLen]byte, err error) {
+	b := make([]byte, nonceLen)
+
+	if _, err = rand.Read(b); err != nil {
+		return
+	}
+
+	h := make([]byte, NonceHexLen)
+
+	_ = hex.Encode(h, b)
+	copy(nonce[:], h[:])
+	return
+}
+
+// VerifyArmoredSig uses an armored public key to verify an armored, detached
+// signature.
+func VerifyArmoredSig(public, sig, file io.Reader) (bool, error) {
+	k, err := openpgp.ReadArmoredKeyRing(public)
+
+	if err != nil {
+		return false, err
+	}
+
+	_, err = openpgp.CheckArmoredDetachedSignature(k, file, sig)
+
+	return err == nil, err
+}
