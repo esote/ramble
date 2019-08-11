43e18d7122e5bdda3ec16ec0f5a39f203d3bd010
esote <esote.net@gmail.com>
2019-08-11 00:16:07 -0500

    Add core mapping of active hello-verify handshakes

--- handshake.go
+++ handshake.go
@@ -1,9 +1,42 @@
 package ramble
 
 import (
+	"errors"
+	"log"
+	"time"
+
 	"github.com/majiru/ramble/internal/pgp"
 	"github.com/majiru/ramble/internal/uuid"
 )
+
+// MaxHVDuration is the max allowed time between the hello request and the
+// expected verify response.
+const MaxHVDuration = time.Minute
+
+func init() {
+	activeHVs = make(map[string]time.Time)
+
+	// Launch globally-persisting goroutine used to prune handshakes older
+	// than MaxHVDuration. The activeHVs value should still be checked each
+	// time since this cannot remove stale handshakes immediately.
+	go func() {
+		ticker := time.NewTicker(MaxHVDuration)
+
+		for {
+			select {
+			case now := <-ticker.C:
+				for uuid, t := range activeHVs {
+					if now.Sub(t) > MaxHVDuration {
+						delete(activeHVs, uuid)
+					}
+				}
+			}
+		}
+	}()
+}
+
+// Map (UUID -> Time added) of active hello-verify handshakes.
+var activeHVs map[string]time.Time
 
 // HelloResponse is sent from the server indicating that it needs verification
 // before continuing.
@@ -15,7 +48,8 @@ type HelloResponse struct {
 	UUID string `json:"uuid"`
 }
 
-// NewHelloResponse generates a hello response with nonce and UUID.
+// NewHelloResponse generates a hello response with nonce and UUID, then adds it
+// to the active hello-verify handshakes map.
 func NewHelloResponse() (*HelloResponse, error) {
 	var h HelloResponse
 
@@ -32,6 +66,14 @@ func NewHelloResponse() (*HelloResponse, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	if t, ok := activeHVs[h.UUID]; ok {
+		log.Printf("UUID %s -> %s already exists in activeHVs!", h.UUID,
+			t.String())
+		return nil, errors.New("the very improbable just happened")
+	}
+
+	activeHVs[h.UUID] = time.Now()
 
 	return &h, nil
 }
